18d056fa261a7c5fa5fe32624d7d3fc434a555dd
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-19 11:56:24 +0200

    feat: added Slack notification

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -32,6 +32,11 @@ data "azurerm_key_vault_secret" "key_vault_bot_token" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+data "azurerm_key_vault_secret" "key_vault_slack_webhook_url" {
+  name         = "slack-webhook-url"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 data "azurerm_resource_group" "nodo_re_rg" {
   name  = "pagopa-${var.env_short}-${local.location_short}-nodo-re-to-datastore-rg"
 }
